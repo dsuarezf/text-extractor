043120b8ca62c8f7376428baacc80f50e37bf6ad
David Suarez <david.suarez.fuentes@gmail.com>
2020-08-09 16:02:50 +0200

    Improve formatting and documentation

--- src/main/python/config.py
+++ src/main/python/config.py
@@ -5,6 +5,39 @@ class Config(object):
 
 class ProductionConfig(Config):
     DEBUG = False
+""" Different configurations to be used by the Flask server
+"""
+
+
+class Config:  # pylint: disable=too-few-public-methods
+    """
+    Common configuration
+    """
+
+
+DEBUG = False
+TESTING = False
+
+
+class ProductionConfig(Config):  # pylint: disable=too-few-public-methods
+    """
+    Production configuration
+    """
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):  # pylint: disable=too-few-public-methods
+    """
+    Development configuration
+    """
+    DEBUG = True
+
+
+class TestingConfig(Config):  # pylint: disable=too-few-public-methods
+    """
+    Testing configuration
+    """
+    TESTING = True
 
 
 class DevelopmentConfig(Config):
